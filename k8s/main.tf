0dcaddcce0e309030878b65acb10deaf9b7b5e14
Steven Robbins <54734080+steven-robbins@users.noreply.github.com>
2023-12-23 16:47:25 +0800

    Adding outputs for nodes

--- k8s/main.tf
+++ k8s/main.tf
@@ -5,9 +5,28 @@ module "k8s-control-plane" {
   key_name = "workstation"
 }
 
-module "k8s-data-plane" {
+module "k8s-data-plane-1" {
   source = "../modules/k8s-data-plane"
   cluster_name = "lab1"
   key_name = "workstation"
   bucket_name = module.k8s-control-plane.bucket_name
+}
+
+module "k8s-data-plane-2" {
+  source = "../modules/k8s-data-plane"
+  cluster_name = "lab1"
+  key_name = "workstation"
+  bucket_name = module.k8s-control-plane.bucket_name
+}
+
+output "control-plane-dns" {
+  module.k8s-control-plane.public-dns
+}
+
+output "data-plane-1-dns" {
+  module.k8s-data-plane-1.public-dns
+}
+
+output "data-plane-2-dns" {
+  module.k8s-data-plane-2.public-dns
 }
