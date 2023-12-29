fc1ce1385b4e2653258a61c3321699c9ca546922
Steven Robbins <54734080+steven-robbins@users.noreply.github.com>
2023-12-29 06:57:03 +0000

    Create lab directory

--- labs/k8s/main.tf
+++ labs/k8s/main.tf
@@ -1,12 +1,12 @@
 
 module "k8s-control-plane" {
-  source       = "../modules/k8s-control-plane"
+  source       = "../../modules/k8s-control-plane"
   cluster_name = "lab1"
   key_name     = "workstation"
 }
 
 module "k8s-data-plane-1" {
-  source       = "../modules/k8s-data-plane"
+  source       = "../../modules/k8s-data-plane"
   cluster_name = "lab1"
   key_name     = "workstation"
   bucket_name  = module.k8s-control-plane.bucket_name
@@ -14,7 +14,7 @@ module "k8s-data-plane-1" {
 }
 
 module "k8s-data-plane-2" {
-  source       = "../modules/k8s-data-plane"
+  source       = "../../modules/k8s-data-plane"
   cluster_name = "lab1"
   key_name     = "workstation"
   bucket_name  = module.k8s-control-plane.bucket_name
